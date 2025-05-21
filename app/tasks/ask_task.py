568a183f800c281e77d3f039c3214c79d0837752
malyushitsky <malyushitsky@yandex.ru>
2025-05-21 09:43:49 +0300

    add query caching

--- app/tasks/ask_task.py
+++ app/tasks/ask_task.py
@@ -1,9 +1,14 @@
-from typing import List
 from celery_app import celery
 from app.db.database import SessionLocal
 from app.db.crud import get_user_arxiv_id
 from app.services.vectorstore import retrieve_and_rerank
 from app.celery_globals import llm, vectorstore, reranker, tokenizer
+from typing import List
+import redis
+import hashlib
+import json
+
+r = redis.Redis(host="redis", port=6379, db=1)
 
 SYSTEM_MSG = (
     "Ты — помощник по научным статьям. "
@@ -20,7 +25,9 @@ def build_messages(question: str, context: str) -> List[dict]:
         {"role": "system", "content": f"Контекст:\n{context}"},
     ]
 
-
+def make_cache_key(arxiv_id: str, question: str) -> str:
+    hash_q = hashlib.sha256(question.encode("utf-8")).hexdigest()
+    return f"qa:{arxiv_id}:{hash_q}"
 
 @celery.task
 def ask_article_task(user_id: str, question: str) -> dict:
@@ -37,6 +44,12 @@ def ask_article_task(user_id: str, question: str) -> dict:
         if not arxiv_id:
             return {"error": "Вы ещё не загрузили статью. Сначала загрузите её, а потом задавайте вопросы!"}
 
+        # Проверка кеша
+        cache_key = make_cache_key(arxiv_id, question)
+        cached = r.get(cache_key)
+        if cached:
+            return json.loads(cached)
+            
         # Поиск релевантных документов
         reranked = retrieve_and_rerank(question, vectorstore, reranker, arxiv_id, top_k=10, top_n=2)
         top_chunks = [doc.page_content for doc, score in reranked]
@@ -48,13 +61,17 @@ def ask_article_task(user_id: str, question: str) -> dict:
 
         # Генерация ответа через LLM
         answer = llm(messages, max_new_tokens=256, temperature=0.7, top_p=0.8, top_k=20, min_p=0)[0]['generated_text']
-
-        return {
+        
+        result = {
             "arxiv_id": arxiv_id,
             "question": question,
             "answer": answer,
             "chunks_used": top_chunks
         }
+
+        # Сохранение кеша на 24ч
+        r.set(cache_key, json.dumps(result, ensure_ascii=False), ex=60 * 60 * 24)  
+        return result
     except Exception as e:
         return {"error": str(e)}
     finally:
--- app/tasks/summarize_task.py
+++ app/tasks/summarize_task.py
@@ -3,6 +3,9 @@ from celery_app import celery
 from app.db.database import SessionLocal
 from app.db.crud import get_user_arxiv_id, get_article_by_id
 from app.celery_globals import llm, tokenizer
+import redis
+import hashlib
+import json
 
 SYSTEM_MSG = (
     "Ты — помощник по научным статьям. "
@@ -18,6 +21,8 @@ def build_messages(article) -> list[dict]:
         {"role": "assistant", "content": "Краткое резюме:"},
     ]
 
+def make_cache_key(arxiv_id: str) -> str:
+    return f"summary:{arxiv_id}"
 
 @celery.task
 def summarize_article_task(user_id: str) -> dict:
@@ -35,25 +40,34 @@ def summarize_article_task(user_id: str) -> dict:
         arxiv_id = get_user_arxiv_id(db, user_id)
         if not arxiv_id:
             return {"error": "У пользователя нет связанной статьи"}
-
+        
         # Получаем метаданные статьи из базы данных
         article = get_article_by_id(db, arxiv_id)
         if not article:
             return {"error": "Статья не найдена"}
 
+        # Проверка кеша
+        cache_key = make_cache_key(arxiv_id)
+        cached = r.get(cache_key)
+        if cached:
+            return json.loads(cached)
+            
         # Используем chat_template
         messages = build_messages(article)
         messages = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, enable_thinking=False)
         summary = llm(messages, max_new_tokens=256, temperature=0.7, top_p=0.8, top_k=20, min_p=0)[0]['generated_text']
 
-        # Возвращаем данные
-        return {
+        result = {
             "arxiv_id": arxiv_id,
             "title": article.title,
             "summary": summary,
             "abstract": article.abstract,
             "conclusion": article.conclusion
         }
+        
+        # Сохранение кеша на 24ч
+        r.set(cache_key, json.dumps(result, ensure_ascii=False), ex=60 * 60 * 24) 
+        return result
 
     finally:
         db.close()
